f992b931f34dff0a286e4c2ee797c712f8ad8806
cabosh <callum.bosher@gmail.com>
2020-05-18 10:20:28 +0100

    Attempted commit

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,9 @@
 def main():
     #write your code below this line
+    year=int(input("Enter a year: "))
+
+    if year < 2015:
+        print("Ancient history!")
 
 if __name__ == '__main__':
     main()
